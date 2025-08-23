0d01b4f385123f1fd411afe0b950a912944adb48
NewbieGanas <146688405+newbieganas@users.noreply.github.com>
2025-08-23 13:00:44 +0700

    Create konversi-nilai.py

---
+++ algoritma-pemrograman/konversi-nilai.py
@@ -0,0 +1,17 @@
+print('Konversi Nilai')
+nilai = float(input('Masukkan Nilai : '))
+
+if nilai >= 80:
+    print('Nilai A')
+elif nilai >= 75:
+    print('Nilai AB')
+elif nilai >= 65:
+    print('Nilai B')
+elif nilai >= 60:
+    print('Nilai BC')
+elif nilai >= 50:
+    print('Nilai C')
+elif nilai >= 40:
+    print('Nilai D')
+else:
+    print('Nilai E')
